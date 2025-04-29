b4d8749ef520377f12c1c61a24102c238c8b0ca6
jorgesalhani <jorge.salhani@usp.br>
2025-04-29 16:10:03 -0300

    engine save fetch results

--- atividade3/engine/engine.py
+++ atividade3/engine/engine.py
@@ -4,7 +4,7 @@ import json
 import os
 
 class Engine:
-  def __init__(self, config_path: str, log_path: str = 'db.log'):
+  def __init__(self, config_path: str, log_path: str = 'db.log', result_path: str = 'result.json'):
     """
     @params
       config_path: caminho para arquivo de configurações de DB
@@ -12,6 +12,7 @@ class Engine:
     """
     self.config_path = config_path
     self.log_path = log_path
+    self.result_path = result_path
     self.connection : LoggingConnection = None
 
   def connect(self):
@@ -30,12 +31,26 @@ class Engine:
     self.connection.autocommit = True
 
   def execute(self, query):
-    cursor = self.connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
+    self.cursor = self.connection.cursor()
+    self.cursor.execute(query)
 
-    return json.dumps(result, indent=2)
+    if ('SELECT' in query):
+      return self.fetchAll()
+    
+    self.cursor.close()
+    return []
   
+  def fetchAll(self):
+    result = self.cursor.fetchall()
+
+    columns = [desc[0] for desc in self.cursor.description]
+    rows = [dict(zip(columns, row)) for row in result]
+    
+    json_result = json.dumps(rows, default=str, indent=2)
+    result_file = open(self.result_path, 'w')
+    result_file.writelines(json_result)
+
+    return json_result
+
   def close(self):
     self.connection.close()
--- atividade3/main.py
+++ atividade3/main.py
@@ -2,13 +2,14 @@ from engine.engine import Engine
 
 connection = Engine('../db_config.json')
 connection.connect()
+queries_folder = './queries'
+query_action = 'select'
+query = 'usuarios.sql'
 
-queries_create_file = open('./queries/create/usuario.sql', 'r')
+sql_folder_queries = f'{queries_folder}/{query_action}/{query}'
+queries_create_file = open(sql_folder_queries, 'r')
 queries = queries_create_file.read()
 
 result = connection.execute(query=queries)
-print(result)
-
-
 
 connection.close()
