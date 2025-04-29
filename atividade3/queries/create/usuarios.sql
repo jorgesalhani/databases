e19cd6491cd5956d97e79e2a25eb5027def2d570
jorgesalhani <jorge.salhani@usp.br>
2025-04-29 16:09:50 -0300

    create new sqls

---
+++ atividade3/queries/create/usuarios.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Usuarios (
+  Cpf varchar(11),
+  Nome varchar(255),
+  Sobrenome varchar(255),
+  ContaBancaria varchar(6),
+  Endereco varchar(255),
+  Sexo varchar(20),
+  Telefone varchar(25),
+  Email varchar(50),
+  Senha varchar(50),
+  DataNascimento Date
+);
---
+++ atividade3/queries/insert/usuarios.sql
@@ -0,0 +1,24 @@
+INSERT INTO Usuarios (
+  Cpf,
+  Nome,
+  Sobrenome,
+  ContaBancaria,
+  Endereco,
+  Sexo,
+  Telefone,
+  Email,
+  Senha,
+  DataNascimento
+)
+VALUES (
+  '000111xxx',
+  'Jorge',
+  'Augusto',
+  '1010x',
+  'Rua Marechal, 1023',
+  'Masculino',
+  '(55)20302220',
+  'jorge@mail.com',
+  '1010xx',
+  '2000-05-10'
+  );
