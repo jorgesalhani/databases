c1543be105e0b97e30bdebb6b593ab540c85aa51
jorgesalhani <jorge.salhani@usp.br>
2025-05-08 10:20:57 -0300

    q5

---
+++ atividade3/queries/select/Q5.sql
@@ -0,0 +1,19 @@
+-- 3.5. Mostre:
+
+-- a. Quais os usuários que são tanto anfitriões quanto locatários
+-- ===============================================================
+
+-- b. Quais anfitriões tiveram pelo menos 5 locações, mostrando seu nome, sua cidade e
+-- quantidade de imóveis dos quais ele é dono, e o total de locações
+-- ===============================================================
+
+-- c. O valor médio das diárias de todas as locações que foram feitas e das que foram
+-- confirmadas em cada mês para o qual exista alguma locação na base
+-- ===============================================================
+
+-- d. Os locatários que são mais jovens do que algum anfitrião
+-- ===============================================================
+
+-- e. Os locatários que são mais jovens do que todos os anfitriões
+-- ===============================================================
+
